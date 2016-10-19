da27ab8c2ef317ac84ffe97789dd9429b2e86420
mimipaskova <maria.paskova@abv.bg>
2016-10-20 00:17:13 +0300

    Add example for dataTypes and readme for Conditionals

---
+++ 2016.10.13_Basics/dataTypes/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    bool isOdd = !(n%2);
+    cout<<isOdd<<endl;
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
